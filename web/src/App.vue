dcf87697cf0bcd382444885e2b74f209365e9017
htrojan <hendrik.trojan@tu-dortmund.de>
2025-01-28 17:20:31 +0100

    refactor: Cleanup default vue template

--- web/src/App.vue
+++ web/src/App.vue
@@ -87,25 +87,22 @@ function simulate_battle_js() {
   <main>
     <div class="flex flex-col justify-center md:flex-row md:justify-normal ">
       <div class="flex flex-col items-center">
-        <div class="text-center">Attacker</div>
+        <div class="text-center text-lg">Attacker</div>
         <ShipDisplay v-for="(ship_if, index) in attacker_ships" :ship-name="ship_if.shipName"
                      v-model:ship="ship_if.ship" v-model:ship-count="ship_if.shipCount" class="m-2"/>
       </div>
       <div class="flex flex-col items-center">
-        <div class="text-center">Defender</div>
+        <div class="text-center text-lg">Defender</div>
         <ShipDisplay v-for="(ship_if, index) in defender_ships" :ship-name="ship_if.shipName"
                      v-model:ship="ship_if.ship" v-model:ship-count="ship_if.shipCount" class="m-2"/>
       </div>
       <div class="flex flex-col justify-center basis-48">
-<!--        <div class="w-full">-->
           <div class="text-center">Results</div>
           <div class="text-center">Defender win: {{ defender_win_percent * 100 }}%</div>
           <div class="text-center">Attacker win: {{ (1 - defender_win_percent) * 100 }}%</div>
         <div class="flex justify-center w-full">
           <button class="shadow-lg  w-24 text-white bg-gray-800 hover:bg-gray-700" @click="simulate_battle_js" type="button">Simulate</button>
         </div>
-<!--        </div>-->
-
       </div>
     </div>
 
---
+++ web/src/simulationWorker.ts
@@ -0,0 +1,5 @@
+self.onmessage = (e: MessageEvent<string>) => {
+
+}
+
+export {};
